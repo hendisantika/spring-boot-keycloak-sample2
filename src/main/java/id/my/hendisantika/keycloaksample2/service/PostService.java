cf240d63ac2b6a520be861ed87fecfa6b740e1b2
hendisantika <hendisantika@yahoo.co.id>
2024-12-13 11:01:55 +0700

    Add deleteTagFromPost in PostService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/service/PostService.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/service/PostService.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.keycloaksample2.service;
 
+import id.my.hendisantika.keycloaksample2.exception.BadRequestException;
 import id.my.hendisantika.keycloaksample2.exception.DataNotFoundException;
 import id.my.hendisantika.keycloaksample2.model.dto.PostDTO;
 import id.my.hendisantika.keycloaksample2.model.entity.Author;
@@ -130,5 +131,15 @@ public class PostService {
                         () ->
                                 new DataNotFoundException(
                                         MessageFormat.format("Post id {0} not found", String.valueOf(postId))));
+    }
+
+    public void deleteTagFromPost(Long postId, Long tagId) {
+        Optional<Post> post = postRepository.findById(postId);
+        if (post.isPresent()) {
+            post.get().removeTag(tagId);
+            postRepository.save(post.get());
+        } else {
+            throw new BadRequestException("Delete error, please check ID and try again");
+        }
     }
 }
