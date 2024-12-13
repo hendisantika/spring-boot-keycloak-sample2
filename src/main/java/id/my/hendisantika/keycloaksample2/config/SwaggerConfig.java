5cacacf77ce8bde66a06f902c4697976caa1ae2f
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:11:25 +0700

    Add apiDocConfig in SwaggerConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/config/SwaggerConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/SwaggerConfig.java
@@ -1,6 +1,16 @@
 package id.my.hendisantika.keycloaksample2.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,5 +45,29 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/swagger-ui", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/api", baseUrl.concat(REDIRECT_URL));
+    }
+
+    @Bean
+    public OpenAPI apiDocConfig() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Spring Boot 3 Keycloak")
+                                .description("")
+                                .version("0.0.1")
+                                .contact(new Contact().name("hendisantika").email("hendisantika@yahoo.co.id")))
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("Documentation")
+                                .url("https://s.id/hendisantika"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        OAUTH_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.OAUTH2)
+                                                .in(SecurityScheme.In.HEADER)
+                                                .flows(new OAuthFlows().password(new OAuthFlow().tokenUrl(authServerUrl)))))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME));
     }
 }
