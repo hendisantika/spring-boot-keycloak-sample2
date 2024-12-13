2a668b31f81f8d1f0dd92fe26c0a37c6be71705d
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:06:05 +0700

    Add getConfigSupplierForUser in RateLimitConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitConfig.java
@@ -1,12 +1,15 @@
 package id.my.hendisantika.keycloaksample2.config;
 
 
+import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.Refill;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 /**
@@ -30,5 +33,12 @@ public class RateLimitConfig {
 
         // Does not always create a new bucket, but instead returns the existing one if it exists.
         return buckets.builder().build(key, configSupplier);
+    }
+
+    private Supplier<BucketConfiguration> getConfigSupplierForUser() {
+        Refill refill = Refill.intervally(20, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(20, refill);
+
+        return () -> (BucketConfiguration.builder().addLimit(limit).build());
     }
 }
