e1b6c67a1a2e35508b3fa8198ffd49d559a72927
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:08:59 +0700

    Add bucket4jCacheResolver in RedisConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/config/RedisConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.keycloaksample2.config;
 
+import com.giffing.bucket4j.spring.boot.starter.config.cache.SyncCacheResolver;
+import com.giffing.bucket4j.spring.boot.starter.config.cache.jcache.JCacheCacheResolver;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.grid.jcache.JCacheProxyManager;
 import org.redisson.config.Config;
@@ -7,6 +9,7 @@ import org.redisson.jcache.configuration.RedissonConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.cache.CacheManager;
 import javax.cache.Caching;
@@ -53,5 +56,14 @@ public class RedisConfig {
     @Bean
     ProxyManager<String> proxyManager(CacheManager cacheManager) {
         return new JCacheProxyManager<>(cacheManager.getCache("cache"));
+    }
+
+    /**
+     * reference: https://github.com/MarcGiffing/bucket4j-spring-boot-starter/issues/73
+     */
+    @Bean
+    @Primary
+    public SyncCacheResolver bucket4jCacheResolver(CacheManager cacheManager) {
+        return new JCacheCacheResolver(cacheManager);
     }
 }
