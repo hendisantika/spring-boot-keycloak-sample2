4260b6d562ffd7eef07bd8cd8b039c8ba10b484c
hendisantika <hendisantika@yahoo.co.id>
2024-12-13 10:43:31 +0700

    Add PostRepository🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample2/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.keycloaksample2.repository;
+
+import id.my.hendisantika.keycloaksample2.model.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/12/24
+ * Time: 10.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
