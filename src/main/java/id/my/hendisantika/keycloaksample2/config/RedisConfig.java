12b210748c346e7d960cc44fe38b568d353fa5a2
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:07:50 +0700

    Add RedisConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.keycloaksample2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/12/24
+ * Time: 06.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    String redisPort;
+
+    @Value("${spring.data.redis.password}")
+    String redisPassword;
+}
