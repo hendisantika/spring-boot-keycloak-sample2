17b17b0e65448fcd9831c55d9697bee0cb837cfc
hendisantika <hendisantika@yahoo.co.id>
2024-12-13 10:57:40 +0700

    Modify Main App🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/SpringBootKeycloakSample2Application.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/SpringBootKeycloakSample2Application.java
@@ -1,12 +1,30 @@
 package id.my.hendisantika.keycloaksample2;
 
+import org.redisson.jcache.JCachingProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.util.Iterator;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringBootKeycloakSample2Application {
 
     public static void main(String[] args) {
+        // list all the caching provider
+        Iterator<CachingProvider> iterator =
+                Caching.getCachingProviders(Caching.getDefaultClassLoader()).iterator();
+        while (iterator.hasNext()) {
+            CachingProvider provider = iterator.next();
+            if (!(provider instanceof JCachingProvider)) {
+                iterator.remove();
+            }
+        }
         SpringApplication.run(SpringBootKeycloakSample2Application.class, args);
     }
 
