b591155a0148f406544a97c5411aad1c98b5fa0c
hendisantika <hendisantika@yahoo.co.id>
2024-12-13 10:59:24 +0700

    Add getById in AuthorService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/service/AuthorService.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/service/AuthorService.java
@@ -1,10 +1,12 @@
 package id.my.hendisantika.keycloaksample2.service;
 
+import id.my.hendisantika.keycloaksample2.exception.DataNotFoundException;
 import id.my.hendisantika.keycloaksample2.model.entity.Author;
 import id.my.hendisantika.keycloaksample2.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 /**
@@ -26,5 +28,14 @@ public class AuthorService {
     public List<Author> getAllAuthors() {
         List<Author> authorList = authorRepository.findAll();
         return authorList;
+    }
+
+    public Author getById(Long id) {
+        return authorRepository
+                .findById(id)
+                .orElseThrow(
+                        () ->
+                                new DataNotFoundException(
+                                        MessageFormat.format("Author id {0} not found", String.valueOf(id))));
     }
 }
