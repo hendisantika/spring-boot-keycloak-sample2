ba99d4c796eddcd437d1a9ff6f66f491bc1da7d9
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:07:17 +0700

    Add aroundAdvice in RateLimitingAspect🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitingAspect.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitingAspect.java
@@ -1,9 +1,20 @@
 package id.my.hendisantika.keycloaksample2.config;
 
+import id.my.hendisantika.keycloaksample2.exception.TooManyRequestsException;
+import id.my.hendisantika.keycloaksample2.utils.HttpUtils;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.text.MessageFormat;
+import java.util.Objects;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +33,23 @@ import org.springframework.stereotype.Component;
 public class RateLimitingAspect {
 
     private final RateLimitConfig rateLimitConfig;
+
+    @Around(value = "execution(* id.my.hendisantika.keycloaksample2.controller..*(..))")
+    public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request =
+                ((ServletRequestAttributes)
+                        Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                        .getRequest();
+        String ip = HttpUtils.getRequestIP(request);
+        Bucket bucket = rateLimitConfig.resolveBucket(ip + "-" + request.getRequestURI());
+        if (bucket.tryConsume(1)) {
+            return joinPoint.proceed();
+        } else {
+            String message =
+                    MessageFormat.format(
+                            "Rate limit exceeded for IP {0} - {1}", String.valueOf(ip), request.getRequestURI());
+            log.warn(message);
+            throw new TooManyRequestsException(message);
+        }
+    }
 }
