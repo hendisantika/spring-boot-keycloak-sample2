3ba2f11ce5c3ba77e15b572e24a368ccd3f1e268
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 06:05:51 +0700

    Add resolveBucket in RateLimitConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample2/config/RateLimitConfig.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.keycloaksample2.config;
 
 
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Supplier;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,11 @@ import org.springframework.context.annotation.Configuration;
 public class RateLimitConfig {
 
     private final ProxyManager buckets;
+
+    public Bucket resolveBucket(String key) {
+        Supplier<BucketConfiguration> configSupplier = getConfigSupplierForUser();
+
+        // Does not always create a new bucket, but instead returns the existing one if it exists.
+        return buckets.builder().build(key, configSupplier);
+    }
 }
